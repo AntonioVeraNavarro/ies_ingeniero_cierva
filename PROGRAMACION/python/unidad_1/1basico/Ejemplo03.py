2f9d78774048ed5ecadc65155e61a6196dae0420
Antonio Vera Navarro <antoniovera.info@gmail.com>
2025-10-06 12:44:46 +0200

    Delete PROGRAMACION directory for rename

--- PROGRAMACION/python/unidad_1/1basico/Ejemplo03.py
+++
@@ -1,4 +0,0 @@
-num1 = 7
-num2 = 3.8
-
-print(f"La suma de ambos numeros es {num1 + num2}")
--- PROGRAMACION/python/unidad_1/1basico/Ejemplo04.py
+++
@@ -1,9 +0,0 @@
-#Forma 1
-print("Introduce tu nombre:")
-name = input()
-print(f"Tu nombre es {name}")
-
-# Forma 2
-age = input("Indica tu edad: ")
-print (f"Tu edad es {age}")
-
